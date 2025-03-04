9cc280dca53cf6ca3523144c9b193b1340b7dfba
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-04 20:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const arr=[1, 2, 3, 4];
+let output=document.getElementById("output")
+function initial(arr){
+	const even=[];
+	return new Promise(resolve=>{
+		for(let t of arr){
+			if(t%2==0){
+				even.push(t)
+			}
+		}
+		resolve(even)
+	})
+	.then(()=>{
+			setTimeout(()=>{
+				output.innerHTML=`${even}`
+			},1000)
+	})
+	.then(()=>{
+		output.innerHTML="";
+		let mult2=[];
+		for(let t of even){
+			mult2.push(t*2)
+		}
+		setTimeout(()=>{
+			output.innerHTML=`${mult2}`
+		},3000)
+	})
+}
+initial(arr)
+
